2aaef5cf7a076574fc832c777ddc12ab0759b2b2
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-24 10:18:12 +0300

    complete assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form() {
+  //state that holds the first name
+  const [firstName, setFirstName] = useState("");
+
+  //state that holds the last name
+  const [lastName, setLastName] = useState("");
+
+  //state that holds data that is submitted by the form
+  //default is an empty array
   const [submittedData, setSubmittedData] = useState([]);
+
+  //state that holds the errors to come
+  //default is an empty array
   const [errors, setErrors] = useState([]);
 
+  //function that gets first name value and 
+  //adjusts the firstName variable by passing it into the firstName function
   function handleFirstNameChange(e) {
     setFirstName(e.target.value);
   }
 
+  //function that gets last name value and 
+  //adjusts the lastName variable by passing it into the lastName function
   function handleLastNameChange(e) {
     setLastName(e.target.value);
   }
 
+//function that handles the onSubmit on the form
 function handleSubmit(event) {
   event.preventDefault();
-  if (firstName.length > 0) {
+  //checks if the firstName and lastName length is greater than 0 or if not passes else 
+  if (firstName.length > 0 && lastName.length > 0) {
     const formData = { 
       firstName: firstName, 
       lastName: lastName };
@@ -26,10 +41,12 @@ function handleSubmit(event) {
     setLastName("");
     setErrors([]);
     } else {
-      setErrors(["First name is required!"]);
+      setErrors(["Fullnames are required!"]);
         }
     }
 
+  //function that iterates through the submittedData array and passes the respective data
+  //for each 
   const listOfSubmissions = submittedData.map((data, index) => {
   return (
     <div key={index}>
@@ -41,13 +58,17 @@ function handleSubmit(event) {
   return (
   <div>
     <form onSubmit={handleSubmit}>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} placeholder="Sylvia"/>
+      <input type="text" onChange={handleLastNameChange} value={lastName} placeholder="Woods"/>
       <button type="submit">Submit</button>
     </form>
+    {/* checks if there is an error that has been passed */}
+    {/* then for each error does the listed function */}
     {errors.length > 0 ? 
-    errors.map((error, index) => 
-    (<p key={index} style={{ color: "red" }}>{error}</p>)) : null}
+    errors.map((error, index) => (<p key={index} style={{ color: "red" }}>{error}</p>)) 
+    : 
+    null}
+    {/* passes the submissions */}
     <h3>Submissions</h3>
     {listOfSubmissions}
   </div>
